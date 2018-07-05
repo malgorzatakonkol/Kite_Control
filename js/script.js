b6154ff880173443709082512ed5e0b7b608d402
Małgorzata Konkol <kalafar.malgorzata@gmail.com>
2018-07-05 18:00:42 +0200

    add API openWeatherMap

--- js/script.js
+++ js/script.js
@@ -35,54 +35,97 @@ $('.slick-slider').slick({
 
 //galeria
 
-var rewaLi = document.getElementById('rewaText');
+// var rewaLi = document.getElementById('rewaText');
+//
+// document.getElementById('rewaPhoto').onmouseenter = function(){
+//     console.log("toooo");
+//     rewaLi.style.display = "inline";
+// };
+//
+// document.getElementById('rewaPhoto').onmouseleave = function(){
+//     console.log("toooo");
+//     rewaLi.style.display = "none";
+// };
+//
+// var hawaiiLi = document.getElementById('hawaiiText');
+//
+// document.getElementById('hawaiiPhoto').onmouseenter = function(){
+//     console.log("toooo");
+//     hawaiiLi.style.display = "inline";
+// };
+//
+// document.getElementById('hawaiiPhoto').onmouseleave = function(){
+//     console.log("toooo");
+//     hawaiiLi.style.display = "none";
+// };
+//
+// var fijiLi = document.getElementById('fijiText');
+//
+// document.getElementById('fijiPhoto').onmouseenter = function(){
+//     console.log("toooo");
+//     fijiLi.style.display = "inline";
+// };
+//
+// document.getElementById('fijiPhoto').onmouseleave = function(){
+//     console.log("toooo");
+//     fijiLi.style.display = "none";
+// };
+//
+// var capetowneLi = document.getElementById('capetownText');
+//
+// document.getElementById('capetownPhoto').onmouseenter = function(){
+//     console.log("toooo");
+//     capetowneLi.style.display = "inline";
+// };
+//
+// document.getElementById('capetownPhoto').onmouseleave = function(){
+//     console.log("toooo");
+//     capetowneLi.style.display = "none";
+// };
 
-document.getElementById('rewaPhoto').onmouseenter = function(){
-    console.log("toooo");
-    rewaLi.style.display = "inline";
-};
 
-document.getElementById('rewaPhoto').onmouseleave = function(){
-    console.log("toooo");
-    rewaLi.style.display = "none";
-};
+$(function () {
 
-var hawaiiLi = document.getElementById('hawaiiText');
+var divWeather = $('.row div');
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?id=";
+var apiKey = '&APPID=bf96a337b9028212ebff87de47bce404';
 
-document.getElementById('hawaiiPhoto').onmouseenter = function(){
-    console.log("toooo");
-    hawaiiLi.style.display = "inline";
-};
 
-document.getElementById('hawaiiPhoto').onmouseleave = function(){
-    console.log("toooo");
-    hawaiiLi.style.display = "none";
-};
 
-var fijiLi = document.getElementById('fijiText');
+    divWeather.on("mouseenter", function (e){
+        var dataWeatherId = $(this).data("id");
+        var thisDiv = $(this);
+        $.ajax({
+            url: weatherUrl + dataWeatherId + apiKey,
+            method: "GET"
+        }).done(function(response) {
+            console.log(response);
+            var temp = response.main.temp;
+            temp = Math.round(temp-273);
+            console.log(temp);
+            thisDiv.find('p.temp').text(temp + " °C");
+            // temp.addClass('info');
 
-document.getElementById('fijiPhoto').onmouseenter = function(){
-    console.log("toooo");
-    fijiLi.style.display = "inline";
-};
+            var desc = response.weather[0].description;
+            console.log(desc);
+            thisDiv.find('p.desc').text(desc);
 
-document.getElementById('fijiPhoto').onmouseleave = function(){
-    console.log("toooo");
-    fijiLi.style.display = "none";
-};
+            var windSpeed = response.wind.speed;
+            console.log(windSpeed);
+            thisDiv.find('p.wind').text("wind " + windSpeed + " m/s");
 
-var capetowneLi = document.getElementById('capetownText');
+            var weatherIcon = response.weather[0].icon;
+            console.log(weatherIcon);
+            thisDiv.find('img#weatherImg').attr('src', 'http://openweathermap.org/img/w/' + weatherIcon + '.png');
+            thisDiv.find('img#weatherImg').attr('id', 'weatherImgg');
+            // weatherIcon.addClass('weatherIcon');
+        }).fail(function(error) {
+            console.log(error);
+        });
+    });
 
-document.getElementById('capetownPhoto').onmouseenter = function(){
-    console.log("toooo");
-    capetowneLi.style.display = "inline";
-};
 
-document.getElementById('capetownPhoto').onmouseleave = function(){
-    console.log("toooo");
-    capetowneLi.style.display = "none";
-};
-
+});
 
 // płynne przejście
 
@@ -96,26 +139,7 @@ $(document).ready(function () {
             event.preventDefault();
             $('html, body').animate({
                 scrollTop: target.offset().top
-            }, 1000);
+            }, 2000);
         }
     });
-//galeria
-
-
-    // var rewaPhoto = $(".firstPhoto");
-    // var rewaText = $(".firstText");
-    // console.log("pierwsze dziecko", rewaPhoto);
-    //
-    // rewaPhoto.on("click", function () {
-    //     console.log(this);
-    //     rewaText.style.display = "none";
-    //  console.log(rewaText);
-    // });
-
-
-    // rewaText.on("mouseover", function(){
-    //     this.style.display = "inline";
-    // })
-
-
 });
