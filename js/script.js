3aec1712f76d3f09a1c0bb09f38cc5ee75d2e118
Małgorzata Konkol <kalafar.malgorzata@gmail.com>
2018-07-06 17:26:13 +0200

    add hamburger

--- js/script.js
+++ js/script.js
@@ -91,4 +91,41 @@ var apiKey = '&APPID=0792ae5427c864ed05425224de36e150';
         }
     });
 
+// hamburger
+
+    $(".mcBurger").click(function() {
+        $(".topsandwich").toggleClass('topsandwichstart');
+        $(".meat").toggleClass("hidden");
+        $(".downsandwich").toggleClass("downsandwichstart");
+        console.log("tooo");
+    });
+
+
+    var menu = document.querySelector('.menu'),
+        menuTrigger = document.querySelector('.mcBurger'),
+        mobile = window.matchMedia('(max-width: 767px)');
+
+    function mobileMatches(mobile) {
+        if (mobile.matches) {
+            onMobile();
+        } else {
+            onDesktop();
+        }
+    }
+    mobileMatches(mobile);
+    mobile.addListener( mobileMatches );
+
+    menuTrigger.addEventListener( 'click', function(){
+        menu.hidden = !menu.hidden;
+    });
+
+    function onMobile(){
+        menu.hidden = true;
+        menuTrigger.hidden = false;
+    }
+
+    function onDesktop(){
+        menu.hidden = false;
+        menuTrigger.hidden = true;
+    }
 });
